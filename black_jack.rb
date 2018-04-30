d43f14b52c47e8ad13dbc366798471b0050af8c5
Savelov-Vitaliy <savelov_vitaliy@mail.ru>
2018-04-30 15:25:10 +0500

    BlackJack correct 1

--- black_jack.rb
+++ black_jack.rb
@@ -44,9 +44,20 @@ class BlackJack
     end
   
     open_cards 
-    game_over?  
+
+    if play_again? 
+      game_over? ?  start_new_game : continue_game 
+    else
+      puts ""
+      puts "Goodbye" 
+    end
   end
 
+  def continue_game
+    reset
+    deal
+  end
+  
   def bets    
     @player.account -= BET
     @dealer.account -= BET
@@ -54,19 +65,13 @@ class BlackJack
   end
 
   def game_over?
-    if @player.account <= 0 || @dealer.account <= 0
-      puts "Game over"
-      print "Play again? (y/n):"
-      case get_player_answer('y', 'n')
-        when 'y' 
-          start_new_game
-        when 'n'
-          puts "Goodbay"
-        end
-    else
-      reset
-      deal
-    end
+    (@player.account <= 0 || @dealer.account <= 0) ? true : false
+  end
+
+  def play_again?
+    puts ""
+    print "Play again? (y/n):"
+    get_player_answer('y', 'n') == 'y' ? true : false
   end
 
   def player_move
@@ -92,25 +97,25 @@ class BlackJack
     print_dealer_cards
   end
 
-   def open_cards
-      puts ""
-      puts "Open cards:"
-      print_dealer_cards(true)
-      print_player_cards
-      puts ""
-      winner = get_winner
-      unless winner.to_s.empty? 
-        winner = instance_variable_get(winner)
-        print "#{winner.name} win! "          
-        winner.account += @bank        
-      else 
-        print "Draw! "
-        @player.account += @bank / 2
-        @dealer.account += @bank / 2        
-      end  
+  def open_cards
+    puts ""
+    puts "Open cards:"
+    print_dealer_cards(true)
+    print_player_cards
+    puts ""
+    winner = get_winner
+    unless winner.to_s.empty? 
+      winner = instance_variable_get(winner)
+      print "#{winner.name} win! "          
+      winner.account += @bank        
+    else 
+      print "Draw! "
+      @player.account += @bank / 2
+      @dealer.account += @bank / 2        
+    end  
 
-      print_accounts     
-   end
+    print_accounts     
+  end
 
   def print_accounts
     puts "#{@player.name}: $#{@player.account}, #{@dealer.name}: $#{@dealer.account}" 
@@ -127,12 +132,12 @@ class BlackJack
   end
 
   def get_winner
-      dealer_points = get_points(@dealer.cards) 
-      player_points = get_points(@player.cards)
-      return nil if (player_points > 21 && dealer_points > 21) || player_points == dealer_points   
-      return :@player if dealer_points > 21 
-      return :@dealer if player_points > 21     
-      return player_points > dealer_points ? :@player : :@dealer
+    dealer_points = get_points(@dealer.cards) 
+    player_points = get_points(@player.cards)
+    return nil if (player_points > 21 && dealer_points > 21) || player_points == dealer_points   
+    return :@player if dealer_points > 21 
+    return :@dealer if player_points > 21     
+    return player_points > dealer_points ? :@player : :@dealer
   end
 
 end
